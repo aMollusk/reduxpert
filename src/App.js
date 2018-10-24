a2173e018dc163b007a49d394d4c4b8089712d72
Kieran McDonald <kieran.mcdonald@qantas.com.au>
2018-10-24 11:43:02 +1100

    Before we abstracted

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createStore, combineReducers } from 'redux';
+import { Provider, connect } from 'react-redux';
 
+
+
+let myState = {
+  counts: {
+    counter: 0
+  }
+}
+
+
+
+let initialState = {
+  counter: 1
+}
+
+function myCoolReducer(state = initialState, action) {
+  switch(action.type) {
+    case 'INCREMENT': {
+      return {
+        ...state,
+        counter: state.counter + 1
+      }
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+let store = createStore(combineReducers({
+  counts: myCoolReducer
+}))
+
+
+// import CoolComponent from './CoolComponent';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Provider store={store}>      
+          <EnhancedMyCoolComponent />
+        </Provider>
       </div>
     );
   }
 }
 
 export default App;
+
+
+const CoolComponent = ({name, counter, ...props}) => {
+  console.log(props)
+  return <div>Hello {name} {counter}
+  
+    <div onClick={() => {
+      props.dispatch({
+        type: 'INCREMENT',
+
+      })
+    }}>click me</div>
+  </div>
+}
+
+const EnhancedMyCoolComponent = connect((state) => {
+  return {
+    name: 'Kieran',
+    ...state.counts
+  }
+})(CoolComponent)
